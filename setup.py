e7b3e01b0a077b493c51f6644e07bc38281eeb42
wb <wowtorx@gmail.com>
2020-03-04 13:26:07 +0100

    fix setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,20 @@
 #!/usr/bin/env python
 
+import os
 from setuptools import setup
+
+
+package_dir = os.path.dirname(__file__)
+requirements_file_path = os.path.join(os.path.dirname(__file__), 'requirements.txt')
+
+with open(requirements_file_path, 'r') as f:
+    packages = [str(f) for f in f.readlines()]
 
 setup(name='lir',
       version='0.0.1',
       description='scripts for calculating likelihood ratios',
-      packages=['liar'],
+      packages=[f'{package_dir}/lir'],
       setup_requires=['nose'],
-      test_suite='nose.collector'
+      test_suite='nose.collector',
+      install_requires=packages,
      )
