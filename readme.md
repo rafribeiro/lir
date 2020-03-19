1b37a906cf25b8cbd118cc31a356be773c89a4cf
rolfypma <rolfypma@gmail.com>
2020-03-19 11:12:09 +0100

    add pypi needed files

---
+++ readme.md
@@ -0,0 +1,2 @@
+
+This library provides a collection of scripts to aid calibration, and calculation and evaluation of Likelihood Ratios
--- setup.py
+++ setup.py
@@ -6,15 +6,25 @@ from setuptools import setup
 
 package_dir = os.path.dirname(__file__)
 requirements_file_path = os.path.join(os.path.dirname(__file__), 'requirements.txt')
-
+print(package_dir)
 with open(requirements_file_path, 'r') as f:
     packages = [str(f) for f in f.readlines()]
 
 setup(name='lir',
       version='0.0.1',
       description='scripts for calculating likelihood ratios',
-      packages=[f'{package_dir}/lir'],
+      url='https://github.com/HolmesNL/lir/',
+      author='Netherlands Forensic Institute',
+      author_email='fbda@holmes.nl',
+      packages=[os.path.join(package_dir,'lir')],
       setup_requires=['nose'],
       test_suite='nose.collector',
       install_requires=packages,
+      classifiers=[
+          'License :: OSI Approved :: Apache Software License',
+          'Programming Language :: Python :: 3 :: Only',
+          'Programming Language :: Python :: 3.6',
+          'Programming Language :: Python :: 3.7',
+          'Programming Language :: Python :: 3.8',
+      ],
      )
