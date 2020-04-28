7070d69c3fee16c15b25aba25d7b72c5aeb620c0
Huub Hoofs (DBS) <huub@holmes.nl>
2020-04-28 16:01:32 +0200

    only support from lir -> sklearn

--- lir/sk_utils.py
+++ lir/sk_utils.py
@@ -3,31 +3,25 @@ from sklearn.pipeline import Pipeline
 from lir import CalibratedScorer, CalibratedScorerCV
 
 
-class CalibratedScorerSk(CalibratedScorer):
-    """
-    As sklearn expects a `predict` in its estimator this class adds the `predict_lr` from the `CalibratedScorer`
-    also as `predict`.
-    In this way the `CalibratedScorer`, can be easily used in sklearn based pipelnes
-    """
-    def predict(self, X):
-        return super().predict_lr(X)
-
-
-class CalibratedScorerCVSk(CalibratedScorerCV):
-    """
-    As sklearn expects a `predict` in its estimator this class adds the `predict_lr` from the `CalibratedScorerCV`
-    also as `predict`.
-    In this way the `CalibratedScorerCV`, can be easily used in sklearn based pipelnes
-    """
-    def predict(self, X):
-        return super().predict_lr(X)
-
-
 class LirPipeline(Pipeline):
     """
     As lir can expects a `predict_lr` in its estimator this class adds the `predict` from the `Pipeline`
     also as `predict_lr`.
     In this way the `Pipeline` from sklearn, can be easily used in an lir based pipelnes
+
+    Parameters
+    ----------
+    X : iterable
+        Data to predict on. Must fulfill input requirements of first step
+        of the pipeline.
+
+    Returns
+    -------
+    y_pred : array-like
+        Likelihood-ratios for the data in X.
     """
     def predict_lr(self, X):
-        return super().predict(self, X)
+        Xt = X
+        for _, name, transform in self._iter(with_final=False):
+            Xt = transform.transform(Xt)
+        return self.steps[-1][-1].predict_lr(Xt)
