86acc3d8e0a7efcac2000cad4590bd9883c76ec5
wowtor <wowtorx@gmail.com>
2020-07-17 13:56:09 +0200

    split lr distribution plotting method into tippett and histogram (#15)

--- lir/plotting.py
+++ lir/plotting.py
@@ -1,6 +1,7 @@
 import collections
 import logging
 import math
+import warnings
 
 import numpy as np
 import matplotlib.pyplot as plt
@@ -376,38 +377,66 @@ def plot_log_lr_distributions_for_model(lr_system: CalibratedScorer, X, y, kind:
 
 
 def plot_log_lr_distributions(lrs, y, kind: str = 'histogram',
-                                        savefig=None, show=None):
+                                        savefig=None, show=None, kw_figure={}):
     """
     plots the 10log lrs
     """
-    kinds = ['histogram', 'tippett']
-    if kind not in kinds:
-        raise ValueError(f'kind should be in {kinds}, got {kind}')
+    warnings.warn('please use plot_lr_histogram or plot_tippett directly', DeprecationWarning)
 
-    plt.figure(figsize=(10,10), dpi=100)
+    kinds = {
+        'histogram': plot_lr_histogram,
+        'tippett': plot_tippett,
+    }
+
+    if kind in kinds:
+        kinds[kind](lrs, y, savefig, show, kw_figure)
+    else:
+        raise ValueError(f'kind should be in {kinds.keys()}, got {kind}')
+
+
+def plot_lr_histogram(lrs, y, savefig=None, show=None, kw_figure={}):
+    """
+    plots the 10log lrs
+    """
+    plt.figure(**kw_figure)
     log_lrs = np.log10(lrs)
-    if kind == 'histogram':
-        points0, points1 = Xy_to_Xn(log_lrs, y)
-        plt.hist(points0, bins=20, alpha=.25, density=True)
-        plt.hist(points1, bins=20, alpha=.25, density=True)
-        plt.xlabel('10log LR')
-    if kind == 'tippett':
-        xplot = np.linspace(np.min(log_lrs), np.max(log_lrs), 100)
-        lr_0, lr_1 = Xy_to_Xn(log_lrs, y)
-        perc0 = (sum(i >= xplot for i in lr_0) / len(lr_0)) * 100
-        perc1 = (sum(i >= xplot for i in lr_1) / len(lr_1)) * 100
 
-        plt.plot(xplot, perc1, color='b', label='LRs given $\mathregular{H_1}$')
-        plt.plot(xplot, perc0, color='r', label='LRs given $\mathregular{H_2}$')
-        plt.axvline(x=0, color='k', linestyle='--')
-        plt.xlabel('Log likelihood ratio')
-        plt.ylabel('Cumulative proportion')
-        plt.legend()
+    points0, points1 = Xy_to_Xn(log_lrs, y)
+    plt.hist(points0, bins=20, alpha=.25, density=True)
+    plt.hist(points1, bins=20, alpha=.25, density=True)
+    plt.xlabel('10log LR')
+
     if savefig is not None:
         plt.savefig(savefig)
-        plt.close()
     if show or savefig is None:
         plt.show()
+    plt.close()
+
+
+def plot_tippett(lrs, y, savefig=None, show=None, kw_figure={}):
+    """
+    plots the 10log lrs
+    """
+    plt.figure(**kw_figure)
+    log_lrs = np.log10(lrs)
+
+    xplot = np.linspace(np.min(log_lrs), np.max(log_lrs), 100)
+    lr_0, lr_1 = Xy_to_Xn(log_lrs, y)
+    perc0 = (sum(i >= xplot for i in lr_0) / len(lr_0)) * 100
+    perc1 = (sum(i >= xplot for i in lr_1) / len(lr_1)) * 100
+
+    plt.plot(xplot, perc1, color='b', label='LRs given $\mathregular{H_1}$')
+    plt.plot(xplot, perc0, color='r', label='LRs given $\mathregular{H_2}$')
+    plt.axvline(x=0, color='k', linestyle='--')
+    plt.xlabel('Log likelihood ratio')
+    plt.ylabel('Cumulative proportion')
+    plt.legend()
+
+    if savefig is not None:
+        plt.savefig(savefig)
+    if show or savefig is None:
+        plt.show()
+    plt.close()
 
 
 def plot_score_distribution_and_calibrator_fit(calibrator, scores, y, savefig=None, show=None):
@@ -428,6 +457,6 @@ def plot_score_distribution_and_calibrator_fit(calibrator, scores, y, savefig=No
 
     if savefig is not None:
         plt.savefig(savefig)
-        plt.close()
     if show or savefig is None:
         plt.show()
+    plt.close()
