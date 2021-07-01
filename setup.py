4c7293e888fc8ec6579bd734dafffa3c8aa703fb
Version bumper <fbda@nfi.nl>
2021-07-01 09:26:22 +0000

    Bump version to 0.0.10

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open("readme.md") as f:
     long_description = f.read()
 setup(
     name="lir",
-    version="0.0.9",
+    version="0.0.10",
     description="scripts for calculating likelihood ratios",
     long_description=long_description,
     long_description_content_type="text/markdown",
